a1a85a4c4bc46610fed5719f24c95605d3069036
Daniel Worthington-Bodart <dan@bodar.com>
2016-12-25 10:01:49 +0000

    Started adding fragmentation test

--- src/server.rs
+++ src/server.rs
@@ -49,7 +49,7 @@ impl Server {
         }
     }
 
-    fn write<'a, W, H>(write: &mut W, handler: &mut H, request:&HttpMessage<'a>) where W: Write + Sized, H: HttpHandler + Sized {
+    fn write<'a, W, H>(write: &mut W, handler: &mut H, request: &HttpMessage<'a>) where W: Write + Sized, H: HttpHandler + Sized {
         let response = handler.handle(&request);
         response.to_write(write);
     }
@@ -109,5 +109,46 @@ impl<H> HttpHandler for LogHandler<H> where H: HttpHandler {
         let response = self.handler.handle(request);
         print!("{}{}\n\n\n", request, response);
         response
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ast::*;
+    use std::io::{Read, Cursor, Result};
+
+    #[test]
+    fn read_supports_fragmentation() {
+        let request = b"GET / HTTP/1.1\r\n\r\n";
+        let mut buffer: [u8; 32] = [0; 32];
+
+        struct Fragmented<'a> {
+            data: Vec<&'a [u8]>,
+            count: usize,
+        }
+
+        impl<'a> Read for Fragmented<'a> {
+            fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+                if self.data.len() == self.count {
+                    return Ok(0)
+                }
+                let fragment = self.data[self.count];
+                buf[..fragment.len()].copy_from_slice(fragment);
+
+                self.count += 1;
+                Ok(fragment.len())
+            }
+        }
+
+        let mut read = Fragmented {
+            data: vec!(&request[..7], &request[8..]),
+            count: 0,
+        };
+
+        assert_eq!(super::Server::read(&mut read, &mut buffer).unwrap(), HttpMessage {
+            start_line: StartLine::RequestLine(RequestLine { method: "GET", request_target: "/", version: HttpVersion { major: 1, minor: 1, } }),
+            headers: Headers(Vec::new()),
+            body: MessageBody::None,
+        });
     }
 }
