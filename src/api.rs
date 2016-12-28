18cd0999ecb413345f48edf9389d797e4b970887
Daniel Worthington-Bodart <dan@bodar.com>
2016-12-28 20:53:25 +0000

    Added Response

--- src/api.rs
+++ src/api.rs
@@ -207,11 +207,89 @@ impl<'a> fmt::Display for Request<'a> {
     }
 }
 
+#[derive(PartialEq, Debug)]
+pub struct Response<'a> {
+    pub code: u16,
+    pub description: &'a str,
+    pub headers: Headers<'a>,
+    pub entity: MessageBody<'a>,
+}
+
+impl<'a> Response<'a> {
+    pub fn new(code: u16, description: &'a str, headers: Headers<'a>, entity: MessageBody<'a>) -> Response<'a> {
+        Response { code: code, description: description, headers: headers, entity: entity }
+    }
+
+    pub fn response(code: u16, description: &'a str) -> Response<'a> {
+        Response::new(code, description, Headers::new(), MessageBody::None)
+    }
+
+    pub fn ok() -> Response<'a> {
+        Response::response(200, "OK")
+    }
+
+    pub fn bad_request() -> Response<'a> {
+        Response::response(400, "Bad Request")
+    }
+
+    pub fn unauthorized() -> Response<'a> {
+        Response::response(401, "Unauthorized")
+    }
+
+    pub fn not_found() -> Response<'a> {
+        Response::response(404, "Not Found")
+    }
+
+    pub fn code(&mut self, code: u16) -> &mut Response<'a> {
+        self.code = code;
+        self
+    }
+
+    pub fn description(&mut self, description: &'a str) -> &mut Response<'a> {
+        self.description = description;
+        self
+    }
+
+    pub fn header(&mut self, name: &'a str, value: &str) -> &mut Response<'a> {
+        self.headers.replace(name, value);
+        self
+    }
+
+    pub fn get_header(&self, name: &str) -> Option<&str> {
+        self.headers.get(name)
+    }
+
+    pub fn remove_header(&mut self, name: &str) -> &mut Response<'a> {
+        self.headers.remove(name);
+        self
+    }
+}
+
+impl<'a> From<HttpMessage<'a>> for Response<'a> {
+    fn from(message: HttpMessage<'a>) -> Response<'a> {
+        if let StartLine::StatusLine(line) = message.start_line {
+            return Response::new(line.code, line.description, message.headers, message.body);
+        }
+        panic!("Can not convert HttpMessage that is a Request into a Response")
+    }
+}
+
+impl<'a> fmt::Display for Response<'a> {
+    fn fmt(&self, format: &mut fmt::Formatter) -> fmt::Result {
+        write!(format, "{}{}\r\n{}",
+               StatusLine { code: self.code, description: self.description, version: HttpVersion { major: 1, minor: 1 } },
+               self.headers,
+               self.entity)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::{Request, Uri};
+
     #[test]
     fn can_parse_uri() {
-        let uri = super::Uri::parse("http://authority/some/path?query=string#fragment");
+        let uri = Uri::parse("http://authority/some/path?query=string#fragment");
         assert_eq!(uri.scheme, Some("http"));
         assert_eq!(uri.authority, Some("authority"));
         assert_eq!(uri.path, "/some/path");
@@ -221,7 +299,7 @@ mod tests {
 
     #[test]
     fn supports_relative() {
-        let uri = super::Uri::parse("some/path");
+        let uri = Uri::parse("some/path");
         assert_eq!(uri.scheme, None);
         assert_eq!(uri.authority, None);
         assert_eq!(uri.path, "some/path");
@@ -231,7 +309,7 @@ mod tests {
 
     #[test]
     fn supports_urns() {
-        let uri = super::Uri::parse("uuid:720f11db-1a29-4a68-a034-43f80b27659d");
+        let uri = Uri::parse("uuid:720f11db-1a29-4a68-a034-43f80b27659d");
         assert_eq!(uri.scheme, Some("uuid"));
         assert_eq!(uri.authority, None);
         assert_eq!(uri.path, "720f11db-1a29-4a68-a034-43f80b27659d");
@@ -242,15 +320,13 @@ mod tests {
     #[test]
     fn is_reverse_able() {
         let original = "http://authority/some/path?query=string#fragment";
-        assert_eq!(super::Uri::parse(original).to_string(), original.to_string());
+        assert_eq!(Uri::parse(original).to_string(), original.to_string());
         let another = "some/path";
-        assert_eq!(super::Uri::parse(another).to_string(), another.to_string());
+        assert_eq!(Uri::parse(another).to_string(), another.to_string());
     }
 
     #[test]
     fn can_pattern_match_a_request() {
-        use super::{Request, Uri};
-
         let request = Request::get("/some/path");
         match request {
             Request { method: "GET", uri: Uri { path: "/some/path", .. }, .. } => {},
