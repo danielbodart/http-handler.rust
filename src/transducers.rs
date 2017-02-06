f796cc31d584999ef1d674dcedd93e84cb4235ac
Daniel Worthington-Bodart <dan@bodar.com>
2017-02-06 09:45:15 +0000

    WIP: Transducers

--- src/transducers.rs
+++ src/transducers.rs
@@ -11,86 +11,86 @@ pub trait Receiver<I, E> {
 }
 
 pub trait Sender<I, E> {
-    fn send(&mut self, receiver: &mut Receiver<I, E>);
+    fn send(&mut self, receiver: Receiver<I, E>);
 }
 
-pub trait Transducer<I, O, E> {
-    type Output: Receiver<I, E>;
-
-    fn apply(&mut self, receiver: &mut Receiver<O, E>) -> Self::Output;
+pub struct MapReceiver<M, R> {
+    receiver: R,
+    mapper: M,
 }
 
-pub struct MapTransducer<I, O, E> {
-    mapper: Fn(Result<I, E>) -> O,
-}
-
-impl<I, O, E> Transducer<I, O, E> for MapTransducer<I, O, E> {
-    type Output = MapReceiver<I, O, E>;
-
-    fn apply(&mut self, receiver: &mut Receiver<O, E>) -> Self::Output {
-        MapReceiver {
-            mapper: self.mapper,
-            receiver: receiver,
-        }
-    }
-
-}
-
-pub struct MapReceiver<I, O, E> {
-    mapper: Box<Fn(Result<I, E>) -> O>,
-    receiver: Receiver<O, E>,
-}
-
-impl<I,O,E> Receiver<I, E>  for MapReceiver<I, O, E>{
+impl<I, O, E, F, R> Receiver<O, E> for MapReceiver<F, R>
+where F: Fn(Result<O, E>) -> Result<I, E>,
+      R: Receiver<I, E> {
     fn start(&mut self) -> State {
-        unimplemented!()
+        self.receiver.start()
     }
 
-    fn next(&mut self, item: Result<I, E>) -> State {
-        unimplemented!()
+    fn next(&mut self, item: Result<O, E>) -> State {
+        self.receiver.next((self.mapper)(item))
     }
 
     fn finish(&mut self) {
-        unimplemented!()
+        self.receiver.finish()
     }
 }
 
-impl<I, E, Iter> Sender<I, E> for Iter where Iter: Iterator<Item = I> {
-    fn send(&mut self, receiver: &mut Receiver<I, E>) {
-        if receiver.start() == State::Stop {
-            return;
+pub trait Transducee<I, O, E, OR, IR> where OR: Receiver<O, E>, IR: Receiver<I, E> {
+    fn apply(self, receiver: OR) -> IR;
+}
+
+pub struct MapTransducee<M> {
+    mapper: M,
+}
+
+impl<I, O, E, F, R> Transducee<I, O, E, R, MapReceiver<F, R>> for MapTransducee<F>
+where F: Fn(Result<I, E>) -> Result<O, E>,
+      R: Receiver<O, E> {
+    fn apply(self, receiver: R) -> MapReceiver<F, R> {
+        MapReceiver {
+            receiver: receiver,
+            mapper: self.mapper,
         }
-        for item in self {
-            if receiver.next(Ok(item)) == State::Stop {
-                break;
-            }
-        }
-        receiver.finish();
     }
 }
 
-pub struct CapturingReceiver<I, E> {
-    items: Vec<Result<I, E>>,
-}
 
-impl<I, E> CapturingReceiver<I, E> {
-    pub fn new() -> CapturingReceiver<I, E> {
-        CapturingReceiver { items: vec![] }
-    }
-}
-
-impl<I, E> Receiver<I, E> for CapturingReceiver<I, E> {
-    fn start(&mut self) -> State {
-        State::Continue
-    }
-
-    fn next(&mut self, item: Result<I, E>) -> State {
-        self.items.push(item);
-        State::Continue
-    }
-
-    fn finish(&mut self) {}
-}
+//impl<I, E, Iter> Sender<I, E> for Iter where Iter: Iterator<Item = I> {
+//    fn send(&mut self, receiver: &mut Receiver<I, E>) {
+//        if receiver.start() == State::Stop {
+//            return;
+//        }
+//        for item in self {
+//            if receiver.next(Ok(item)) == State::Stop {
+//                break;
+//            }
+//        }
+//        receiver.finish();
+//    }
+//}
+//
+//pub struct CapturingReceiver<I, E> {
+//    items: Vec<Result<I, E>>,
+//}
+//
+//impl<I, E> CapturingReceiver<I, E> {
+//    pub fn new() -> CapturingReceiver<I, E> {
+//        CapturingReceiver { items: vec![] }
+//    }
+//}
+//
+//impl<I, E> Receiver<I, E> for CapturingReceiver<I, E> {
+//    fn start(&mut self) -> State {
+//        State::Continue
+//    }
+//
+//    fn next(&mut self, item: Result<I, E>) -> State {
+//        self.items.push(item);
+//        State::Continue
+//    }
+//
+//    fn finish(&mut self) {}
+//}
 
 //pub fn assert_received<I, E, S>(sender: &mut S, items: Vec<I>)
 //    where I: Debug, S: Sender<I, E> {
@@ -102,13 +102,13 @@ impl<I, E> Receiver<I, E> for CapturingReceiver<I, E> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    //    use super::*;
 
     #[test]
     fn can_map() {
-        let items = vec![1, 2];
-        let mut receiver = CapturingReceiver::new();
-        Sender::<_, ()>::send(&mut items.iter().cloned(), &mut receiver);
-        assert_eq!(receiver.items.into_iter().filter_map(|result| result.ok()).collect::<Vec<_>>(), items);
+        //        let items = vec![1, 2];
+        //        let mut receiver = CapturingReceiver::new();
+        //        Sender::<_, ()>::send(&mut items.iter().cloned(), &mut receiver);
+        //        assert_eq!(receiver.items.into_iter().filter_map(|result| result.ok()).collect::<Vec<_>>(), items);
     }
 }
